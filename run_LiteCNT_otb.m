b24e8cf20c9f85b730b1f044328c6f1be0801963
Lingxiao.Yang <zjjconan@gmail.com>
2019-03-30 21:35:38 +0800

    bacup 20190330

--- run_LiteCNT_otb.m
+++ run_LiteCNT_otb.m
@@ -1,19 +1,18 @@
 clc; clear all; close all;
 warning off;
 
-opts.videoPath = 'D:/Dataset/Video/tc128/';
+opts.videoPath = 'D:/Dataset/Video/OTB/';
 opts.savePath = 'F:\Research\tracker_zoo\Evaluation\results/My-Work/LiteCRT/';
 opts.netPath = 'backnet/vggm-conv1.mat';
 
-load('otb_distribute.mat');
 for learningRate = [1e-5]
-    opts.trackerName = ['tc128_LiteCRTv3_dataaug_' num2str(learningRate)];
-    opts.videoAttr = 'tc128';
+    opts.trackerName = ['otb100_LiteCRTv3_dataaug_' num2str(learningRate)];
+    opts.videoAttr = 'OTB2015';
     opts.verbose = false;
     opts.useGpu = true;
     opts.saveResult = true;
     opts.videoList = [];
-    opts.settingFcn = @(x) setting_lcnt_forfun(x, learningRate, 3);
+    opts.settingFcn = @(x) setting_lcnt_forfun(x, learningRate, 2);
     opts.trackerFcn = @tracker_OPE;
 
     [~, opts.runFileName, ~] = fileparts(mfilename('fullpath'));
